caadb8057971328234c436dd5ba690128aff1155
abhishandy <abhishek.sandy@gmail.com>
2020-08-27 20:25:46 -0400

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Second wave of COVID-19 cases in Rensselaer county of New York state
+
+Since colleges and schools are opening for the fall semester, many students have travelled to Rensselaer county from other counties and states. With increased mobility and population density among the students, it is expected that our county will experience a second wave of COVID-19 cases. This website is a humble attempt to track report cases and show the timeline.
+
+Data is picked up from the tweets for [Rensselaer county executive](https://twitter.com/RenscoExecutive)
---
+++ plots.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(5,3))
+cases = np.loadtxt('cases.dat')
+dates = ['Aug {:d}'.format(i) for i in np.arange(cases.shape[0])+24] # starting from Aug 24
+plt.plot(dates, cases, 'ko-')
+plt.tick_params(direction='in')
+plt.xlabel('Date')
+plt.xlabel('New cases')
+plt.savefig('new_cases.png', dpi=300)
+
+plt.figure(figsize=(5,3))
+cases = np.loadtxt('cases.dat')
+dates = ['Aug {:d}'.format(i) for i in np.arange(cases.shape[0])+24] # starting from Aug 24
+plt.plot(dates, np.cumsum(cases), 'ko-')
+plt.tick_params(direction='in')
+plt.xlabel('Date')
+plt.xlabel('Cumulative count')
+plt.savefig('total_cases.png', dpi=300)
+
+plt.show()
