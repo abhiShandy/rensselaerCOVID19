02cd680a713b4bfef7d539d4a87d5ef20262cf25
abhishandy <abhishek.sandy@gmail.com>
2020-09-01 15:14:24 -0400

    added new cases, using pandas, changed layout

--- plots.py
+++ plots.py
@@ -1,22 +1,19 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(5,3))
-cases = np.loadtxt('cases.dat')
-dates = ['Aug {:d}'.format(i) for i in np.arange(cases.shape[0])+24] # starting from Aug 24
-plt.plot(dates, cases, 'ko-')
-plt.tick_params(direction='in')
-plt.xlabel('Date')
-plt.xlabel('New cases')
-plt.savefig('new_cases.png', dpi=300, bbox_inches='tight')
+df = pd.read_csv('cases.csv')
 
-plt.figure(figsize=(5,3))
-cases = np.loadtxt('cases.dat')
-dates = ['Aug {:d}'.format(i) for i in np.arange(cases.shape[0])+24] # starting from Aug 24
-plt.plot(dates, np.cumsum(cases), 'ko-')
-plt.tick_params(direction='in')
+plt.figure(figsize=(10,3))
+plt.plot(df['date'], df['new-cases'], 'b')
 plt.xlabel('Date')
-plt.xlabel('Cumulative count')
-plt.savefig('total_cases.png', dpi=300, bbox_inches='tight')
+plt.ylabel('New Cases', color='b')
+plt.tick_params('y', colors='b')
 
+ax = plt.twinx()
+ax.plot(df['date'], np.cumsum(df['new-cases']), 'r')
+ax.set_ylabel('Cumulative', color='r')
+ax.tick_params(colors='r')
+ax.tick_params(colors='r')
+plt.savefig('cases.png', dpi=300, bbox_inches='tight')
 plt.show()
