5ae3dd757c7cd91d9a0664fa4fc3e9bec66b75ce
abhishandy <abhishek.sandy@gmail.com>
2020-09-03 17:04:34 -0400

    switch to bar plot for new cases, change colors

--- plots.py
+++ plots.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('cases.csv')
 
 plt.figure(figsize=(10,3))
-plt.plot(df['date'], df['new-cases'], 'b')
+sns.barplot('date', 'new-cases', data=df, color='royalblue', alpha=0.5)
 plt.xlabel('Date')
-plt.ylabel('New Cases', color='b')
-plt.tick_params('y', colors='b')
+plt.ylabel('New Cases', color='royalblue')
+plt.tick_params('y', colors='royalblue')
 
 ax = plt.twinx()
-ax.plot(df['date'], np.cumsum(df['new-cases']), 'r')
-ax.set_ylabel('Cumulative', color='r')
-ax.tick_params(colors='r')
-ax.tick_params(colors='r')
+ax.plot(df['date'], np.cumsum(df['new-cases']), 'tomato', marker='o')
+ax.set_ylabel('Cumulative', color='tomato')
+ax.tick_params(colors='tomato')
+ax.tick_params(colors='tomato')
 plt.savefig('cases.png', dpi=300, bbox_inches='tight')
-plt.show()
