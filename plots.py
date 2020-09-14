46e8cdff36170088efb14d206a94ea75faf0a063
abhishandy <abhishek.sandy@gmail.com>
2020-09-14 08:44:58 -0400

    add new cases, horizonal grid lines

--- plots.py
+++ plots.py
@@ -10,19 +10,19 @@ plt.figure(figsize=(10,3))
 sns.barplot('date', 'new-cases', data=df, color='royalblue', alpha=0.5)
 plt.xlabel('Date')
 plt.ylabel('New Cases', color='royalblue')
-plt.tick_params('y', colors='royalblue')
+plt.tick_params('y', colors='royalblue', direction='in')
+plt.grid(True, axis='y', which='major', ls=':', c='royalblue')
 
 ax = plt.twinx()
 ax.plot(df['date'], np.cumsum(df['new-cases']), 'tomato', marker='o')
 ax.set_ylabel('Cumulative', color='tomato')
 ax.tick_params(colors='tomato')
 ax.tick_params(colors='tomato')
-
 # automatically adjust tick labels
 Ndates = len(df['date'])
 showNdates = 10
 labels = np.array(df['date'])[::Ndates//showNdates]
 ax.set_xticks(labels)
-
 plt.savefig('cases.png', dpi=300, bbox_inches='tight')
+plt.show()
 
